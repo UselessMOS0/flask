ab399dfc354ae7d1103873ea8ac183b3bea3a1b2
UselessMOS0 <bono.walter@iisgalvanimi.edu.it>
2022-02-24 10:59:46 +0000

    ciao

--- static/js/timeEs1.js
+++ static/js/timeEs1.js
@@ -1,9 +1,11 @@
 const now = new Date()
-const end = new Date(2022,6,8)
+const end = new Date("2022/6/8")
+console.log(end.getMonth());
 
 function cd() {
     let delta = end - now
     document.getElementById('time').innerHTML = `${Math.floor(delta / 86400000)} days, ${Math.floor(delta % 86400000 / 3600000)} hours and ${Math.floor(delta % 86400000 % 3600000 / 60000)} minutes left until the end of school`
+    console.log(delta)
 }
 
 cd()
