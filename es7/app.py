1a3b8a4bf471c31d27ff7e0e27cb7983349c6dc7
UselessMOS0 <bono.walter@iisgalvanimi.edu.it>
2022-03-31 09:53:11 +0000

    sss

--- es7/app.py
+++ es7/app.py
@@ -2,8 +2,14 @@ from flask import Flask, render_template, Response, request
 app = Flask(__name__)
 
 import pandas as pd
+import io 
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+from matplotlib.figure import Figure
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
 
-stazioni = pd.read_csv('es7/templates/coordfix_ripetitori_radiofonici_milano_160120_loc_final.csv',sep=';')
+stazioni = pd.read_csv('templates/coordfix_ripetitori_radiofonici_milano_160120_loc_final.csv',sep=';')
 
 @app.route('/', methods=['GET'])
 def home():
@@ -11,11 +17,22 @@ def home():
 
     
 @app.route('/numero', methods=['GET'])
-def home():
+def num():
+    global risultato
     # numero stazioni per ogni municipio
     risultato = stazioni[['OPERATORE','MUNICIPIO']].groupby('MUNICIPIO',as_index=False).count().sort_values(by='MUNICIPIO',ascending=True)
     return  render_template('elenco.html',risultato = risultato.to_html())
 
+@app.route('/grafico', methods=['GET'])
+def grafico():
+    fig, ax = plt.subplots(figsize=(10,10))
+
+    ax.bar(risultato.MUNICIPIO,risultato.OPERATORE)
+
+    output = io.BytesIO()
+    FigureCanvas(fig).print_png(output)
+    return Response(output.getvalue(),mimetype='image/png')
+    
     
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=3245, debug=True)
