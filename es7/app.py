52671e56f8b2d4a541dcadb6141c186a0b2885c9
UselessMOS0 <bono.walter@iisgalvanimi.edu.it>
2022-04-05 11:16:07 +0000

    aasasas

--- es7/app.py
+++ es7/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, Response, request, redirect
+from flask import Flask, render_template, Response, request, redirect, url_for
 app = Flask(__name__)
 
 import pandas as pd
@@ -9,26 +9,29 @@ from matplotlib.figure import Figure
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
+import contextily as ctx
 
 stazioni = pd.read_csv('templates/coordfix_ripetitori_radiofonici_milano_160120_loc_final.csv',sep=';')
-stazionigeo = gpd.read_file('templates/coordfix_ripetitori_radiofonici_milano_160120_loc_final.csv')
+stazionigeo = gpd.read_file('https://dati.comune.milano.it/dataset/7fae4996-02e1-4a80-8794-9ec22454041b/resource/eec5e0ef-2622-443e-a2e4-8b066af2e093/download/ds710_coordfix_ripetitori_radiofonici_milano_160120_loc_final.geojson')
+quartieri = gpd.read_file('/workspace/flask/templates/es5/ds964_nil_wm-20220322T110843Z-001.zip')
+print(quartieri)
 
 @app.route('/', methods=['GET'])
 def home():
-    if request.method == 'GET':
+    return  render_template('index.html')
 
-        return  render_template('index.html')
+@app.route('/redirect', methods=['GET'])
+def redir():
+    if request.args['opt'] == '1':
+        return redirect(url_for('numero'))
+    elif request.args['opt'] == '2':
+        return redirect(url_for('input'))
     else:
-        if request.form.get['opt'] == '1':
-            redirect(url_for('numero'))
-        elif request.form.get['opt'] == '2':
-            redirect(url_for('input'))
-        else:
-            redirect(url_for(''))
+        return redirect(url_for('dropdown'))
 
     
 @app.route('/numero', methods=['GET'])
-def num():
+def numero():
     global risultato
     # numero stazioni per ogni municipio
     risultato = stazioni[['OPERATORE','MUNICIPIO']].groupby('MUNICIPIO',as_index=False).count().sort_values(by='MUNICIPIO',ascending=True)
@@ -51,8 +54,33 @@ def input():
 
 @app.route('/ricerca', methods=['GET'])
 def ricerca():
-
-    return
+    global u_quart, stazioni_quartiere
+    u_quart = quartieri[quartieri.NIL.str.contains(request.args['quartiere'])]
+    stazioni_quartiere = stazionigeo[stazionigeo.within(u_quart.geometry.squeeze())]
+    return  render_template('elenco1.html',stazioni_quart = stazioni_quartiere.to_html())
     
+
+@app.route('/mappa', methods=['GET'])
+def mappa():
+    fig, ax = plt.subplots(figsize=(10,10))
+
+    u_quart.to_crs(3857).plot(alpha=.5)
+    stazioni_quartiere.to_crs(3857).plot(color='k',ax=ax)
+    ctx.add_basemap(ax=ax)
+
+    output = io.BytesIO()
+    FigureCanvas(fig).print_png(output)
+    return Response(output.getvalue(),mimetype='image/png')
+
+@app.route('/dropdown', methods=['GET'])
+def dropdown():
+    nomi_stazioni = stazioni.OPERATORE.drop_duplicates().sort_values(ascending=True).to_list()
+    return render_template('dropdown.html', stazioni = nomi_stazioni)
+
+@app.route('/sceltastazioni', methods=['GET'])
+def sceltastazioni():
+    stazione = stazioni[stazioni.OPERATORE.str.contains(request.args['stazione'])]
+    return render_template('vistastazione.html', stazione = stazione.to_html())
+
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=3245, debug=True)
