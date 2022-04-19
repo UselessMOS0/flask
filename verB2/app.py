3e24e44ce17e3b43011adf7f1e94791ab655bd7f
UselessMOS0 <bono.walter@iisgalvanimi.edu.it>
2022-04-19 20:05:52 +0000

    abc

--- verB2/app.py
+++ verB2/app.py
@@ -10,12 +10,23 @@ import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 
+
+province['AREA'] = province.geometry.area
+
 @app.route('/', methods=['GET'])
 def home():
   return  render_template('index.html')
   
+@app.route('/ricerca', methods=['GET'])
+def home():
+  return  render_template('ricerca.html')
 
-
+@app.route('/mappa', methods=['GET'])
+def home():
+  u_reg = regioni[regioni.REGIONE.str.contains(request.args['u_reg'])]
+  lung = u_reg.geometry.length/1000
+  prov_in_reg = province[province.within(u_reg.geometry.squeeze())].sort_values(by='AREA',ascending=False)['PROVINCIA']
+  return render_template('mappa.html')
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=3245, debug=True)
